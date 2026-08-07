67fe6c4cd95a55febd29acda4055f8ef5dbb814d
Renan Goncalves <renan.goncalves@otima.digital>
2026-08-07 08:20:25 -0300

    chore: log laravel runtime failures safely

--- api/api/index.php
+++ api/api/index.php
@@ -5,7 +5,17 @@ use Illuminate\Http\Request;
 
 define('LARAVEL_START', microtime(true));
 require __DIR__.'/../vendor/autoload.php';
-/** @var Application $app */
-$app = require_once __DIR__.'/../bootstrap/app.php';
-$app->handleRequest(Request::capture());
+try {
+    /** @var Application $app */
+    $app = require_once __DIR__.'/../bootstrap/app.php';
+    $app->handleRequest(Request::capture());
+} catch (Throwable $exception) {
+    error_log(sprintf(
+        'Loterias Online request failure: %s at %s:%d',
+        get_class($exception),
+        basename($exception->getFile()),
+        $exception->getLine(),
+    ));
 
+    throw $exception;
+}
